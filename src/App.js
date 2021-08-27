7e173d11c8a4812bd85451d4cca8a98c567abfdc
alifzuhairi <76780196+alifzuhairi@users.noreply.github.com>
2021-08-27 15:15:33 +0800

    experience

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Intro from './components/Intro';
 import Skills from './components/Skills';
+import Education from './components/Education';
 import Particles from 'react-particles-js';
 
 function App() {
@@ -25,7 +26,9 @@ function App() {
    <Navbar/>
    <Header/>
    <Intro/>
+   <Education/>
    <Skills/>
+   
     </>
   );
 }
---
+++ src/components/Education.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Education = () => {
+    return (
+        <div className="educations" id="edu">
+
+<div className="split left">
+                <div className="centered">
+         <h2 className="project-title">Education</h2>
+
+        <div className="box2">
+    <p> <img src="assets/SKRMM.png" className="schoolimg"></img> SK Raja Muda Musa // UPSR ( 3A2B )</p>
+    <p> <img src="assets/SKRMM.png" className="schoolimg"></img>SMK Raja Muda Musa // PT3 ( 5A2B1C )</p>
+    <p> <img src="assets/MCKK.png" className="schoolimg"></img>Malay College Kuala Kangsar // SPM ( 5A4B )</p>
+    <p> <img src="assets/UPM.png" className="schoolimg"></img>Universiti Putra Malaysia // Foundation ( 3.51 )</p>
+    <p> <img src="assets/UPM.png" className="schoolimg"></img>Universiti Putra Malaysia // Degree ( 3.14 )</p>
+        </div>
+            </div>
+            </div>
+
+            <div className="split right">
+                <div className="centered">
+                <h2 className="project-title">Working Experience</h2>
+                <div className="box2">
+                <p> <img src="assets/padi.png" className="schoolimg"></img>Certified PADI Divemaster ( 2018 - Now )</p>
+                <p> <img src="assets/MCKK.png" className="schoolimg"></img>Assistant Swimming Instructor ( 2017 - 2018 )</p>
+                <p> <img src="assets/youtube1.png" className="schoolimg"></img>Content Creator // Youtuber ( 2020 - Now )</p>
+                <p> <img src="assets/web.png" className="schoolimg"></img>Freelance Web Developer ( 2020 - Now )</p>
+
+
+
+                </div>
+            </div>
+            </div>
+
+         </div>
+    )
+}
+
+export default Education
